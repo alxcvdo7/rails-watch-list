d1c90426bf05b415a38b5e40bd5e609f6a1512c4
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-05-16 11:04:25 -0400

    all views created

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,21 +1,45 @@
 class BookmarksController < ApplicationController
+  before_action :find_list, only: [:new, :create]
+  before_action :find_bookmark, only: :destroy
+
   def new
-    raise
     @bookmark = Bookmark.new
   end
 
   def create
-    # find the list to create the bookmark
-    @list = List.find(params[:list_id])
-    # create the bookmark
+    # create bookmark with strong params
     @bookmark = Bookmark.new(bookmark_params)
-    #
+    # find the list where the bookmark is created and assign it to the bookmark
     @bookmark.list = @list
-    # redirect to the list
-    # redirect_to list_path(@list)
+    if @bookmark.save
+      # if bookmark is saved go to show page
+      redirect_to list_path(@list)
+    else
+      # or redirect to the new template
+      render :new
+    end
+
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  # identify the bookmark
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  # strong params to make a valid bookmark
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  # find the list in which the bookmarks are created/deleted
+  def find_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
@@ -16,16 +18,22 @@ class ListsController < ApplicationController
     # create a list
     @list = List.new(list_params)
     # save the list
-    list.save
+    if @list.save
     # redirect the user to the list
-    redirect_to list_path(@list)
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
     # need an if?  with invalid params,
     # - assigns a newly created but unsaved list as @list (FAILED - 14)
     # - re-renders the 'new' template (FAILED - 15).
-
   end
 
   private
+
+  def find_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
